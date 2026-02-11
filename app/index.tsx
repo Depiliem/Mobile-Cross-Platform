a0092bd1ea4ee6374792e31d80d7c811c7824a95
Depiliem <davew1726@gmail.com>
2026-02-11 15:13:55 +0700

    week02

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
@@ -8,8 +8,6 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Text>Dave Wiliam 7.</Text>
-    </View>
+    ></View>
   );
 }
