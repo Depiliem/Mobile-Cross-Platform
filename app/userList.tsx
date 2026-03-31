9da0ebb9f3340491a990819bc4443edb96a0b8b4
Depiliem <davew1726@gmail.com>
2026-03-31 19:35:07 +0700

    Week07

--- app/userList.tsx
+++ app/userList.tsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
 import { ScrollView, TouchableOpacity, View } from "react-native";
 import { Avatar, Card, Text } from "react-native-paper";
+import Animated, { FadeInDown } from "react-native-reanimated";
 import styles from "./AppStyles";
 import userData from "./data.json";
 
@@ -8,31 +9,33 @@ export default function userList() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {userData.map((user, index) => (
-        <Card key={index} style={styles.card}>
-          <Link
-            href={{
-              pathname: "/profile",
-              params: {
-                name: user.name,
-                email: user.email,
-                photo: user.photo_url,
-              },
-            }}
-            asChild
-          >
-            <TouchableOpacity>
-              <Card.Content style={styles.cardContent}>
-                <Avatar.Image size={70} source={{ uri: user.photo_url }} />
-                <View style={styles.textContainer}>
-                  <Text variant="titleMedium" style={styles.boldText}>
-                    {user.name}
-                  </Text>
-                  <Text variant="bodyMedium">{user.email}</Text>
-                </View>
-              </Card.Content>
-            </TouchableOpacity>
-          </Link>
-        </Card>
+        <Animated.View key={index} entering={FadeInDown.delay(index * 150)}>
+          <Card style={styles.card}>
+            <Link
+              href={{
+                pathname: "/profile",
+                params: {
+                  name: user.name,
+                  email: user.email,
+                  photo: user.photo_url,
+                },
+              }}
+              asChild
+            >
+              <TouchableOpacity>
+                <Card.Content style={styles.cardContent}>
+                  <Avatar.Image size={70} source={{ uri: user.photo_url }} />
+                  <View style={styles.textContainer}>
+                    <Text variant="titleMedium" style={styles.boldText}>
+                      {user.name}
+                    </Text>
+                    <Text variant="bodyMedium">{user.email}</Text>
+                  </View>
+                </Card.Content>
+              </TouchableOpacity>
+            </Link>
+          </Card>
+        </Animated.View>
       ))}
     </ScrollView>
   );
